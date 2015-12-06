808844ea8c58fec165a6d1fc3104ca5b9fdeaaf5
doczong <mkmorris@gmail.com>
2015-12-06 13:35:26 -0500

    Remove broken Subscenter (no longer un Subliminal as well)

--- couchpotato/core/plugins/subtitle.py
+++ couchpotato/core/plugins/subtitle.py
@@ -16,7 +16,7 @@ autoload = 'Subtitle'
 
 class Subtitle(Plugin):
 
-    services = ['opensubtitles', 'thesubdb', 'subswiki', 'subscenter']
+    services = ['opensubtitles', 'thesubdb', 'subswiki']
 
     def __init__(self):
         addEvent('renamer.before', self.searchSingle)
